1c6645eac316147b7ae8a7138f46006442b4c001
Samuel Viana <samuel.viana.yl.gmail.com>
2024-05-13 18:24:36 -0300

    Adi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import ListaSuspensa from './components/ListaSuspensa';
 
 function App() {
+
   return (
     <div className='App'>
       <Banner />
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,14 +1,38 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const cargos = [
+        'Mestre',
+        'Player'
+    ]
+
+    const mesas = [
+        'Mandleton',
+        'Hunter ao Cubo',
+        'O Capítulo Vermelho',
+        'Conhecimento Infinito',
+        'Personagens'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card dos membros do grupo</h2>
-                <CampoTexto label='Nome' placeholder='Digite seu nome' />
-                <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
+                <CampoTexto obrigatorio={true} label='Nome' placeholder='Digite seu nome' />
+                <ListaSuspensa label='Cargo' itens={cargos}/>
                 <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem' />
+                <ListaSuspensa obrigatorio={true} label='Categoria' itens={mesas} />
+                <Botao>
+                    Criar card
+                </Botao>
             </form>
         </section>
     )
